c7925566dd95a0c7e3c123aa8b9c17a2bc225704
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-06-07 17:36:43 -0300

    Quesito 9

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// const { fetchProducts } = require('./helpers/fetchProducts');
-
-// const getSavedCartItems = require("./helpers/getSavedCartItems");
-
-// const saveCartItems = require("./helpers/saveCartItems");
-
 // const { fetchItem } = require("./helpers/fetchItem");
-
+const list = document.querySelector('.cart__items');
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -35,23 +29,47 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {};
+const payPrice = () => {
+  let cost = 0;
+  const listItems = document.getElementsByClassName('cart__item');
+  for (let index = 0; index < listItems.length; index += 1) {
+  const item = (document.getElementsByClassName('cart__item')[index]).innerText;
+  const price = Number(item.split('$')[1]);
+  console.log(price);
+  cost += price;
+}
+const other = document.getElementsByClassName('total-price');
+if (other.length > 0) {
+other[0].remove();
+}
+const priceHtml = document.createElement('div');
+priceHtml.className = 'total-price';
+const priceString = cost.toString();
+priceHtml.innerText = priceString;
+const cart = document.querySelector('.cart');
+cart.appendChild(priceHtml);
+};
+
 let counter = 0;
 if (JSON.parse(localStorage.getItem('counter')) === null) {
   counter = 0;
 } else {
   counter = JSON.parse(localStorage.getItem('counter'));
 }
-const createCartItemElement = ({ sku, name, salePrice }) => {
+
+const createCartItemElement = async ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', () => {
+  li.addEventListener('click', async () => {
     li.remove();
     localStorage.clear();
     const others = document.getElementsByClassName('cart__item');
     for (let index = 0; index < others.length; index += 1) {
      localStorage.setItem(index, others[index].innerHTML);
     }
+    cost = 0;
+    await payPrice();
   });
   return li;
 };
@@ -66,30 +84,33 @@ const printProducts = async () => {
       image: thumbnail,
     };
     const item = createProductItemElement(product);
-    const list = document.querySelector('.items');
-    list.appendChild(item);
+    const lista = document.querySelector('.items');
+    lista.appendChild(item);
   });
 };
 
-const printPrice = async () => {
-  const obj = await fetchProducts('computador');
-  const array = obj.results;
+const envelope = async (array) => {
   array.forEach(async ({ id, title }, index) => {
     const button = document.getElementsByClassName('item__add')[index];
     const itemSku = (document.getElementsByClassName('item__sku')[index]).innerText;
     const priceObj = await fetchItem(itemSku);
     const { price } = priceObj;
     const objData = { sku: id, name: title, salePrice: price };
-    const li = createCartItemElement(objData);
-    button.addEventListener('click', () => {
-      const list = document.querySelector('.cart__items');
+    const li = await createCartItemElement(objData);
+    button.addEventListener('click', async () => {
       list.appendChild(li);
       saveCartItems(li.innerHTML);
       const items = getSavedCartItems('cartItems');
       localStorage.setItem(counter, items);
       counter += 1;
+      payPrice();
     });
   });
+};
+const printPrice = async () => {
+  const obj = await fetchProducts('computador');
+  const array = obj.results;
+  await envelope(array);
 };
 
 const get = () => {
@@ -97,8 +118,7 @@ const get = () => {
     const nova = getSavedCartItems(index);
     const item = document.createElement('li');
     item.innerText = nova;
-    if (nova !== null) {      
-      const list = document.querySelector('.cart__items');
+    if (nova !== null) { 
       list.appendChild(item);
       item.addEventListener('click', () => {
         item.remove();
@@ -108,6 +128,7 @@ const get = () => {
   }
   localStorage.removeItem('cartItems');
 };
+
 localStorage.setItem('counter', JSON.stringify(counter));
 window.onload = async () => {
   await printProducts();
